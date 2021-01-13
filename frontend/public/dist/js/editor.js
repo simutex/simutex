a3f3576fdc230f0d39e936315b838df7f741dcf2
noahdavis319 <noahdavis319@gmail.com>
2021-01-12 20:25:40 -0500

    fixed title on blur

--- frontend/public/dist/js/editor.js
+++ frontend/public/dist/js/editor.js
@@ -1,21 +1,24 @@
 /* Project Title Editing */
 $(document).on("dblclick", "#project_title", function () {
-    var old_project_title = $("#project_title").val().trim();
+    let old_project_title = $("#project_title").text().trim();
+
     function updateTitle() {
+        console.log("out");
         var new_project_name = $("#new_project_title").val().trim();
         if (new_project_name == "") {
-            new_project_name = current;
+            $("#project_title").text(old_project_title);
+        } else {
+            $.post('./settings/rename', { data: new_project_name }, (data) => {
+                if (data == "M") {
+                    $("#project_title").text(new_project_name);
+                } else {
+                    $("#project_title").text(old_project_title);
+                }
+            });
         }
-        $.post('./settings/rename', { data: new_project_name }, (data) => {
-            if (data == "M") {
-                $("#project_title").text(new_project_name);
-            } else {
-                $("#project_title").text(old_project_name);
-            }
-        });
     }
 
-    var current = $(this).text();
+    let current = $(this).text();
     $("#project_title").html(`<form class="form-inline" onsubmit="return false;">
         <input id="new_project_title" class="form-control mr-sm-2" type="search" placeholder="${current}" aria-label="Search">
     </form>`);
