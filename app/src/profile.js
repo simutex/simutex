753870d00404f2bbde5a90406ee628d31a8321bc
coryjk <36049396+coryjk@users.noreply.github.com>
2021-01-19 22:49:14 -0800

    Ability to set display name and change passwords
    If no display name is present for an account, it will default to their username.
    When their password is changed, after passing all validation checks, the user's
    cookies will be cleared and they will need to login again using their new password.

--- app/src/profile.js
+++ app/src/profile.js
@@ -17,10 +17,37 @@ const auth = require('./auth');
 router.use(auth.middleware.credentials);
 router.use('/profile', auth.middleware.credentials); 
 
-router.get('/', auth.middleware.credentials, (req, res) => {
+/**
+ * Queries for any pre-existing display name. If not set, defaults to username.
+ * 
+ * @param {String} username 
+ */
+async function getDisplayNameOrDefault(username) {
+    const accounts = await db.get().collection('accounts').find({ username: username }).toArray();
+    return accounts.length > 0 && accounts[0].displayName ? accounts[0].displayName : username;
+}
+
+/**
+ * Constructs EJS variables to be used while rendering the edit profile page.
+ * 
+ * @param {String} username 
+ */
+async function getProfilePageData(username) {
+    const displayName = await getDisplayNameOrDefault(username);
     ejs_vars = {
-        user: req.cookies.u,
+        user: username,
+        displayName: displayName,
+        error: ""
     };
+    return ejs_vars;
+}
+
+/**
+ * Attempts to retrieve the display name for a given user. If attribute not present, simply
+ * defaults to the actual username.
+ */
+router.get('/', auth.middleware.credentials, async (req, res) => {
+    ejs_vars = await getProfilePageData(req.cookies.u);
     ejs.renderFile('app/views/profile.ejs', ejs_vars, {}, (err, str) => {
         res.send(str);
     });
@@ -30,37 +57,70 @@ router.get('/', auth.middleware.credentials, (req, res) => {
  * Modify user display name from profile page.
  */
 router.post('/changename', auth.middleware.credentials, (req, res) => {
-    const newUsername = req.body.newUsername;
-    if (newUsername && newUsername != req.cookies.u) {
+    const newDisplayName = req.body.newDisplayName;
+    if (newDisplayName) {
         // handles response
         function handle(err, callback) {
             res.send(err ? 'E' : 'M');
         }
-        
-        // check if username is available
-        db.get().collection('accounts').find({ username: newUsername }).toArray((err, accounts) => {
-            if (accounts.length > 0) {
-                res.send('D');
-            }
-        });
-
-        // change username
-        db.get().collection('accounts').updateOne({ username: req.cookies.u }, { $set: { username: newUsername } }, (err, res) => {
-            if (err) {
-                res.send('M');
-            }
-        });
-
-        // change matching names
-        db.get().collection('projects').updateOne({ owner: })
+        // change display name
+        db.get().collection('accounts')
+            .updateOne({ username: req.cookies.u }, { $set: { displayName: newDisplayName } }, handle);
     }
 });
 
 /**
  * Change password from profile page and update user cookie.
  */
-router.post('/changepassword', auth.middleware.credentials, (req, res) => {
+router.post('/changepassword', auth.middleware.credentials, async (req, res) => {
+    const oldPassword = req.body.oldPassword;
+    const newPassword = req.body.newPassword1;
 
+    /*
+     * inner function to populate ejs variables with error reason, which will
+     * be noticed by the frontend and rendered accordingly
+     */
+    const ejs_vars = await getProfilePageData(req.cookies.u);
+    function fail(ejs_vars, reason) {
+        ejs_vars.error = reason;
+        console.log(ejs_vars);
+        ejs.renderFile('app/views/profile.ejs', ejs_vars, {}, (err, str) => {
+            res.send(str);
+        });
+    }
+
+    /*
+     * inner function to handle clearing cookies upon successfully setting
+     * new password
+     */
+    function handle(err, callback) {
+        if (!err) {
+            // clear session and redirect to login
+            res.clearCookie('u');
+            res.clearCookie('h');
+            res.redirect('/login');
+        } else {
+            // something went wrong...
+            console.log(err);
+        }
+    }
+
+    // failed due to wrong password
+    const hashword = crypto.createHash("sha512").update(oldPassword, 'utf-8').digest('hex');
+    if (req.cookies.h != hashword) {
+        fail(ejs_vars, "Your old password was wrong!");
+        return;
+    }
+
+    // failed due to invalid new password
+    if (!oldPassword || !newPassword || oldPassword == newPassword) {
+        fail(ejs_vars, "Your new password cannot be the same as your old password!");
+        return;
+    }
+
+    // set new password
+    const newHashword = crypto.createHash("sha512").update(newPassword, 'utf-8').digest('hex');
+    db.get().collection('accounts').updateOne({ username: req.cookies.u }, { $set: { password: newHashword } }, handle);
 });
 
 module.exports = {
--- frontend/public/dist/js/edit-profile.js
+++ frontend/public/dist/js/edit-profile.js
@@ -2,23 +2,75 @@ $(() => {
     $('[data-toggle="tooltip"]').tooltip()
 });
 
+// alphanumeric with _, 3 to 16 characters long
+const validDisplayName = /^[a-zA-Z0-9_]{2,15}$/;
 
-function changeUsername(oldUsername, newUsername) {
-    // must be a different username
-    if (oldUsername && newUsername && newUsername.toLowerCase() != oldUsername.toLowerCase()) {
-        // send request
-        $.post('/profile/changename', { newUsername: newUsername }, (response) => {
-            if (response == "M") {
-                location.reload();
-            } else {
-                console.log("Could not update account name.");
-            }
-        });
+// placeholder password requirements
+const validPassword = /^.*/;
+const validPasswordRequirements = "TODO";
+
+/**
+ * After performing simple validation checks sends a post request for display name
+ * changing in backend.
+ * 
+ * @param {String} oldDisplayName 
+ * @param {String} newDisplayName 
+ */
+function changeDisplayName(oldDisplayName, newDisplayName) {
+    // must use valid format
+    if (!oldDisplayName || !newDisplayName || !validDisplayName.test(newDisplayName)) {
+        alert("Your display name must be alphanumeric and between 3 and 16 characters long.");
     } else {
-        alert("You must enter a new name!");
+        // must be a different username
+        if (newDisplayName.toLowerCase() != oldDisplayName.toLowerCase()) {
+            // send request
+            $.post('/profile/changename', { newDisplayName: newDisplayName }, (response) => {
+                if (response == "M") {
+                    location.reload();
+                } else {
+                    console.log("Could not update account display name.");
+                }
+            });
+        } else {
+            alert("You must enter a new name!");
+        }
     }
 }
 
-function changePassword(hashword, oldPassword, newPassword, confirmNewPassword) {
-    
+function isValidPassword(newPassword, confirmNewPassword) {
+    if (newPassword && confirmNewPassword && newPassword == confirmNewPassword) {
+        if (validPassword.test(newPassword)) {
+            return true;
+        }
+        alert("Your new password must meet the following requirements: " + validPasswordRequirements);
+    } else {
+        alert("New and confirmation password don't match!");
+    }
+    return false;
 }
+
+// /**
+//  * After input validation sends post request to backend to further process request.
+//  * No request is made if confirmation does not match or if 
+//  * 
+//  * @param {String} oldPassword 
+//  * @param {String} newPassword 
+//  * @param {String} confirmNewPassword 
+//  */
+// function changePassword(oldPassword, newPassword, confirmNewPassword) {
+//     if (oldPassword && newPassword && confirmNewPassword && newPassword == confirmNewPassword) {
+//         // validate password requirements
+//         if (!validPassword.test(newPassword)) {
+//             alert("Your password is not strong enough!");
+//         } else {
+//             // send request
+//             $.post('/profile/changepassword', { oldPassword: oldPassword, newPassword: newPassword }, (response) => {
+//                 if (response == 'M') {
+//                     location.reload();
+//                 } else {
+//                     console.log("Could not update password.");
+//                 }
+//             });
+//         }
+//     }
+// }
